230187634dd8410c4e6830fa7496927de2dfdfb6
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-02-13 03:10:20 -0500

    Make home content easier to read

--- src/App.js
+++ src/App.js
@@ -23,20 +23,22 @@ export default class App extends Component {
 
         <section id="home">
           <h1>Dragon<wbr/>Hacks!</h1>
-          <div>Drexel's very own 24-hour hackathon!</div>
-          <div>
-            <a href="https://goo.gl/maps/EQj1FVXS1UG2">
-              <span className="fas fa-map-marker-alt prepend"/>
-              <span>
-                Bossone Research Enterprise Center
-              </span>
-            </a>
-          </div>
-          <div>
-            <span className="fas fa-calendar prepend"/>
-            <time dateTime="2019-04-20">
-              April 20<sup>th</sup>&ndash;21<sup>st</sup> {App.year}
-            </time>
+          <div className="quick-info">
+            <div>Drexel's very own 24-hour hackathon!</div>
+            <div>
+              <a href="https://goo.gl/maps/EQj1FVXS1UG2">
+                <span className="fas fa-map-marker-alt prepend"/>
+                <span>
+                  Bossone Research Enterprise Center
+                </span>
+              </a>
+            </div>
+            <div>
+              <span className="fas fa-calendar prepend"/>
+              <time dateTime="2019-04-20">
+                April 20<sup>th</sup>&ndash;21<sup>st</sup> {App.year}
+              </time>
+            </div>
           </div>
         </section>
 
