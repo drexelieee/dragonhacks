c0b306ce7ebe73fea723e374a72e4cc790524950
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-02-19 04:22:27 -0500

    Add Balsamiq sponsor

--- src/app.js
+++ src/app.js
@@ -182,68 +182,83 @@ export default class App extends Component {
         </section>
 
 
-        <section id="sponsors">
-          <header>
-            <h2>Sponsors</h2>
-          </header>
-          <Sponsor link="https://www.ieee.org/"
-            logo={require("./img/logo_ieee.png")}
-            alt="IEEE UPP"/>
-          <Sponsor link="https://drexel.edu/engineering/"
-            logo={require("./img/logo_drexel_ece.png")}
-            alt="Drexel ECE Dept."/>
-          <Sponsor link="https://drexel.edu/provost/"
-            logo={require("./img/logo_drexel_provost.jpg")}
-            alt="Drexel Office of the Provost"/>
-          <Sponsor link="http://www.lutron.com/en-US/Pages/default.aspx"
-            logo={require("./img/logo_lutron.jpg")}
-            alt="Lutron"/>
-          <Sponsor link="https://www.lockheedmartin.com/en-us/index.html"
-            logo={require("./img/logo_lockheed.svg")}
-            alt="Lockheed Martin"/>
-          <Sponsor link="https://boomi.com/"
-            logo={require("./img/logo_dell_boomi.svg")}
-            alt="Dell Boomi"/>
-          <Sponsor link="https://investor.vanguard.com/corporate-portal/"
-            logo={require("./img/logo_vanguard.svg")}
-            alt="Vanguard"/>
-          <Sponsor link="https://www.jetbrains.com/"
-            logo={require("./img/logo_jetbrains.png")}
-            alt="JetBrains"/>
-          <Sponsor link="https://www.hedera.com/"
-            logo={require("./img/logo_hedera.png")}
-            alt="Hedera Hashgraph"/>
-          <Sponsor link="https://www.sketchapp.com/"
-            logo={require("./img/logo_sketch.png")}
-            alt="Sketch"/>
-        </section>
+        <div className="bg-transition">
+          <section id="sponsors">
+            <header>
+              <h2>Sponsors</h2>
+            </header>
+            <div>
+              <div className="package gold">
+                <Sponsor link="https://www.ieee.org/"
+                  logo={require("./img/logo_ieee.png")}
+                  alt="IEEE UPP"/>
+                <Sponsor link="https://drexel.edu/engineering/"
+                  logo={require("./img/logo_drexel_ece.png")}
+                  alt="Drexel ECE Dept."/>
+                <Sponsor link="https://drexel.edu/provost/"
+                  logo={require("./img/logo_drexel_provost.jpg")}
+                  alt="Drexel Office of the Provost"/>
+              </div>
+              <div className="package silver">
+                <Sponsor link="http://www.lutron.com/en-US/Pages/default.aspx"
+                  logo={require("./img/logo_lutron.png")}
+                  alt="Lutron"/>
+                <Sponsor link="https://www.lockheedmartin.com/en-us/index.html"
+                  logo={require("./img/logo_lockheed.svg")}
+                  alt="Lockheed Martin"/>
+                <Sponsor link="https://boomi.com/"
+                  logo={require("./img/logo_dell_boomi.svg")}
+                  alt="Dell Boomi"/>
+              </div>
+              <div className="package bronze">
+                <Sponsor link="https://investor.vanguard.com/corporate-portal/"
+                  logo={require("./img/logo_vanguard.svg")}
+                  alt="Vanguard"/>
+                <Sponsor link="https://balsamiq.com/"
+                  logo={require("./img/logo_balsamiq.svg")}
+                  alt="Balsamiq"/>
+              </div>
+              <div className="package none">
+                <Sponsor link="https://www.jetbrains.com/"
+                  logo={require("./img/logo_jetbrains.png")}
+                  alt="JetBrains"/>
+                <Sponsor link="https://www.hedera.com/"
+                  logo={require("./img/logo_hedera.png")}
+                  alt="Hedera Hashgraph"/>
+                <Sponsor link="https://www.sketchapp.com/"
+                  logo={require("./img/logo_sketch.png")}
+                  alt="Sketch"/>
+              </div>
+            </div>
+          </section>
 
 
-        <section id="sponsorUs">
-          <header>
-            <h2>Sponsor Us!</h2>
-          </header>
-          <p>
-            Please support our hackathon! Benefits include: The ability to
-            recruit from the hackathon participants, gaining feedback and new
-            users from participants using your products, and recognition for your
-            company. More information is in the link to the right. Please
-            contact <a href="mailto:teamdragonhacks@gmail.com">teamdragonhacks@gmail.com</a> to
-            begin sponsoring!
-          </p>
-          <a href={require('./pdf/Dragon_Hacks_2018_Sponsorship_Document.pdf')}>
-            Full Sponsorship information
-          </a>
-          <div>
-            In partnership with
-          </div>
-          <img src={require("./img/logo_mlh.svg")} width="300" alt="MLH"/>
-        </section>
+          <section id="sponsorUs">
+            <header>
+              <h2>Sponsor Us!</h2>
+            </header>
+            <p>
+              Please support our hackathon! Benefits include: The ability to
+              recruit from the hackathon participants, gaining feedback and new
+              users from participants using your products, and recognition for your
+              company. More information is in the link to the right. Please
+              contact <a href="mailto:teamdragonhacks@gmail.com">teamdragonhacks@gmail.com</a> to
+              begin sponsoring!
+            </p>
+            <a href={require('./pdf/Dragon_Hacks_2018_Sponsorship_Document.pdf')}>
+              Full Sponsorship information
+            </a>
+            <div>
+              In partnership with
+            </div>
+            <img src={require("./img/logo_mlh.svg")} width="300" alt="MLH"/>
+          </section>
 
 
-        <footer id="pageFooter">
+          <footer id="pageFooter">
           Copyright &copy; {App.year} Technical Chairs, Drexel IEEE. All Rights Reserved.
         </footer>
+        </div>
       </div>
     );
   }
