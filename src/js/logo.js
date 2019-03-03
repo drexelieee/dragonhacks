64196d82e873adfb007eb061120bf4f010b3ab53
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-03-03 16:07:40 -0500

    Reformat leading whitespace

--- src/js/logo.js
+++ src/js/logo.js
@@ -8,8 +8,8 @@ export default class Logo extends Component {
       <div className="logo">
         <a className="logo__link" href={this.props.href}>
           <img className="logo__image"
-               src={require(this.props.imgSrc)}
-               alt={this.props.altText}/>
+            src={require(this.props.imgSrc)}
+            alt={this.props.altText}/>
         </a>
       </div>
     );
