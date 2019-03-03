1c30af40b144eed719c65f94c07b7a8a5556a3e8
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-03-03 14:59:58 -0500

    Add logo block

---
+++ src/js/logo.js
@@ -0,0 +1,17 @@
+import '../css/logo.css';
+
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <div className="logo">
+        <a className="logo__link" href={this.props.href}>
+          <img className="logo__image"
+               src={require(this.props.imgSrc)}
+               alt={this.props.altText}/>
+        </a>
+      </div>
+    );
+  }
+}
