11e1906bc8af02a835efab1d9811a89b900d7be8
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-03-03 14:21:55 -0500

    Refactor text block

--- src/js/text.js
+++ src/js/text.js
@@ -4,19 +4,11 @@ import React, { Component } from 'react';
 
 export default class Text extends Component {
   render() {
-    if (this.props.inline) {
-      var jsx = (
-        <span className="text">
-          {this.props.children}
-        </span>
-      );
-    } else {
-      var jsx = (
-        <p className="text">
-          {this.props.children}
-        </p>
-      );
-    }
-    return jsx;
+    const TopLevelTag = this.props.inline ? 'span' : 'p';
+    return (
+      <TopLevelTag className="text">
+        {this.props.children}
+      </TopLevelTag>
+    );
   }
 }
