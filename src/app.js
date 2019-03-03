05c3ea73e5abeb7f344111f84b2cb80014f9545d
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-03-03 14:54:19 -0500

    Remove unused imports

--- src/app.js
+++ src/app.js
@@ -4,12 +4,6 @@ import React, { Component } from 'react';
 import NavBar from './js/section/navbar';
 import Section from './js/section';
 import Text from './js/text';
-import Home from './js/section/home';
-import About from './js/section/about';
-import Schedule from './js/section/schedule';
-import FAQ from './js/section/faq';
-import Sponsors from './js/section/sponsors';
-import SponsorUs from './js/section/sponsor_us';
 
 export default class App extends Component {
   static data = {
