aab6e218a8e8181da5520ee6a99080b3444ef401
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-03-20 15:11:59 -0400

    Make navlinks smooth scroll to sections

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -32,7 +32,7 @@ export default class NavBar extends Component {
 
   render() {
     return (
-      <div className="navbar">
+      <div id="page-navbar" className="navbar">
         <div className="navbar__logo">
           <Text inline bigger>
             Dragon<Text inline bigger accent>Hacks</Text>
@@ -83,21 +83,45 @@ class NavBarList extends Component {
     return (
       <nav className={rootClasses}>
         <div className="navbar__list-item">
-          <Clickable navLink href="#home">Home</Clickable>
+          <Clickable navLink href="#home" onClick={scrollToId('home')}>
+            Home
+          </Clickable>
         </div>
         <div className="navbar__list-item">
-          <Clickable navLink href="#about">About</Clickable>
+          <Clickable navLink href="#about" onClick={scrollToId('about')}>
+            About
+          </Clickable>
         </div>
         <div className="navbar__list-item">
-          <Clickable navLink href="#schedule">Schedule</Clickable>
+          <Clickable navLink href="#schedule" onClick={scrollToId('schedule')}>
+            Schedule
+          </Clickable>
         </div>
         <div className="navbar__list-item">
-          <Clickable navLink href="#faq">FAQ</Clickable>
+          <Clickable navLink href="#faq" onClick={scrollToId('faq')}>
+            FAQ
+          </Clickable>
         </div>
         <div className="navbar__list-item">
-          <Clickable navLink href="#sponsors">Sponsors</Clickable>
+          <Clickable navLink href="#sponsors" onClick={scrollToId('sponsors')}>
+            Sponsors
+          </Clickable>
         </div>
       </nav>
     );
   }
+}
+
+function scrollToId(id) {
+  let elm = document.getElementById(id);
+  let pageNavBar = document.getElementById('page-navbar');
+  return (event) => {
+    event.preventDefault();
+    elm.focus();
+    window.scrollTo({
+      left: 0,
+      top: elm.offsetTop - pageNavBar.offsetHeight,
+      behavior: 'smooth',
+    });
+  };
 }
