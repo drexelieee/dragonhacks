7157269fc4d7c94460be0fd25865475bb69e4a87
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-04-13 18:07:53 -0400

    Add ScheduleRow component

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -10,7 +10,7 @@ import Logo from './Logo';
 import SponsorTier from './SponsorTier';
 import FAQ from './Faq';
 import Clickable from './Clickable';
-import Schedule from './Schedule';
+import Schedule, { ScheduleRow } from './Schedule';
 
 export default class App extends Component {
   static data = {
@@ -66,7 +66,31 @@ export default class App extends Component {
           </Text>
         </Section>
         <Section id="schedule" heading="Schedule">
-          <Schedule/>
+          <Schedule>
+            <ScheduleRow time="2019-04-20T09:00" event="Registration"/>
+            <ScheduleRow time="2019-04-20T11:00" event="Team Building 1"/>
+            <ScheduleRow time="2019-04-20T12:00" event="Team Building 2"/>
+            <ScheduleRow time="2019-04-20T13:00" event="Lunch &amp; Network"/>
+            <ScheduleRow time="2019-04-20T14:00" event="Opening Ceremony"/>
+            <ScheduleRow time="2019-04-20T15:00" event="Tech Workshop 1"/>
+            <ScheduleRow time="2019-04-20T16:00" event="Tech Workshop 2"/>
+            <ScheduleRow time="2019-04-20T17:00" event="Tech Workshop 3"/>
+            <ScheduleRow time="2019-04-20T18:00" event="Tech Workshop 4"/>
+            <ScheduleRow time="2019-04-20T19:00" event="Dinner"/>
+            <ScheduleRow time="2019-04-20T19:30" event="Arcade Opens!"/>
+            <ScheduleRow time="2019-04-20T20:00" event="Entertainment"/>
+          </Schedule>
+          <Schedule>
+            <ScheduleRow time="2019-05-20T00:00" event="More Entertainment"/>
+            <ScheduleRow time="2019-05-20T01:00" event="Midnight Pizza"/>
+            <ScheduleRow time="2019-05-20T09:00" event="Breakfast"/>
+            <ScheduleRow time="2019-05-20T13:00" event="Lunch"/>
+            <ScheduleRow time="2019-05-20T14:00" event="Devpost Submissions"/>
+            <ScheduleRow time="2019-05-20T15:00" event="Hacking Ends"/>
+            <ScheduleRow time="2019-05-20T16:00" event="Judging Begins"/>
+            <ScheduleRow time="2019-05-20T17:00" event="Closing Ceremony"/>
+            <ScheduleRow time="2019-05-20T17:30" event="Return Equipment"/>
+          </Schedule>
         </Section>
         <Section id="faq" heading="FAQ">
           <FAQ question="Is there a code of conduct?">
--- src/js/component/Schedule.js
+++ src/js/component/Schedule.js
@@ -6,100 +6,37 @@ import Text from './Text';
 export default class Schedule extends Component {
   render() {
     return (
-      <table>
+      <table className="schedule">
         <thead>
           <tr>
-            <th><Text inline>Time</Text></th>
-            <th><Text inline>Event</Text></th>
+            <th scope="col"><Text inline>Time</Text></th>
+            <th scope="col"><Text inline>Event</Text></th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td><time dateTime="9:00"><Text inline>9:00 AM</Text></time></td>
-            <td><Text inline>Registration</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="11:00"><Text inline>11:00 AM</Text></time></td>
-            <td><Text inline>Team Building 1</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="12:00"><Text inline>12:00 PM</Text></time></td>
-            <td><Text inline>Team Building 2</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="13:00"><Text inline>1:00 PM</Text></time></td>
-            <td><Text inline>Lunch &amp; Network</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="14:00"><Text inline>2:00 PM</Text></time></td>
-            <td><Text inline>Opening Ceremony</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="15:00"><Text inline>3:00 PM</Text></time></td>
-            <td><Text inline>Tech Workshop 1</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="16:00"><Text inline>4:00 PM</Text></time></td>
-            <td><Text inline>Tech Workshop 2</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="17:00"><Text inline>5:00 PM</Text></time></td>
-            <td><Text inline>Tech Workshop 3</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="18:00"><Text inline>6:00 PM</Text></time></td>
-            <td><Text inline>Tech Workshop 4</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="19:00"><Text inline>7:00 PM</Text></time></td>
-            <td><Text inline>Dinner</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="19:30"><Text inline>7:30 PM</Text></time></td>
-            <td><Text inline>Arcade Opens!</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="20:00"><Text inline>8:00 PM</Text></time></td>
-            <td><Text inline>Entertainment</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="00:00"><Text inline>12:00 AM</Text></time></td>
-            <td><Text inline>More Entertainment</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="1:00"><Text inline>1:00 AM</Text></time></td>
-            <td><Text inline>Midnight Pizza</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="9:00"><Text inline>9:00 AM</Text></time></td>
-            <td><Text inline>Breakfast</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="13:00"><Text inline>1:00 PM</Text></time></td>
-            <td><Text inline>Lunch</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="14:00"><Text inline>2:00 PM</Text></time></td>
-            <td><Text inline>Devpost Submissions</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="15:00"><Text inline>3:00 PM</Text></time></td>
-            <td><Text inline>Hacking Ends</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="16:00"><Text inline>4:00 PM</Text></time></td>
-            <td><Text inline>Judging Begins</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="17:00"><Text inline>5:00 PM</Text></time></td>
-            <td><Text inline>Closing Ceremony</Text></td>
-          </tr>
-          <tr>
-            <td><time dateTime="17:30"><Text inline>5:30 PM</Text></time></td>
-            <td><Text inline>Return Equipment</Text></td>
-          </tr>
+          {this.props.children}
         </tbody>
       </table>
+    );
+  }
+}
+
+export class ScheduleRow extends Component {
+  render() {
+    return (
+      <tr className="schedule__row">
+        <td><time dateTime={this.props.time}>
+          <Text inline>
+            {new Date(this.props.time).toLocaleTimeString(
+              navigator.language,
+              {hour: 'numeric', minute: '2-digit'}
+            )}
+          </Text>
+        </time></td>
+        <td>
+          <Text inline>{this.props.event}</Text>
+        </td>
+      </tr>
     );
   }
 }
